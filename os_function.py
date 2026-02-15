594c354f08d228ff96158642997231b688568531
Z580 <artvid29@gmail.com>
2026-02-15 13:52:31 +0300

    Убрано дублирование строки

--- os_function.py
+++ os_function.py
@@ -4,3 +4,5 @@ import os
 os.listdir('.')
 # ['.git', '.idea', '.venv', 'os_function.py', 'README.md'] - получен список папок и файлов в текущей
 # директори , включая скрытые
+
+os.rename('_testfile', 'testfile') # переименование файла  '_testfile' на 'testfile'
