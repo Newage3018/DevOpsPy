869e8319f1df0499e5e69b58c566d3bf92647e9c
Z580 <artvid29@gmail.com>
2026-02-15 13:41:06 +0300

    начало

--- os_function.py
+++ os_function.py
@@ -1,1 +1,8 @@
 # начало
+import os
+
+os.listdir('.')
+
+os.listdir('.')
+# ['.git', '.idea', '.venv', 'os_function.py', 'README.md'] - получен список папок и файлов в текущей
+# директори , включая скрытые
